1447e24c6aa72913f9b08592d5ece41f1e3f8535
Agata Piątek <a.piatek100@gmail.com>
2021-08-06 11:29:54 +0200

    Images changes

--- main.js
+++ main.js
@@ -5,8 +5,19 @@ const images = [{
     img: "img/bread.jpg"
 },
 {
-    img: "breakfast.jpg"
+    img: "img/breakfast.jpg"
 }];
 
-const img = document.querySelector('pictures');
+const picture = document.querySelector('img');
 
+const time = 5000;
+let active = 0;
+
+const changeImage = () => {
+    active++;
+    if (active === images.length) {
+        active = 0;
+    }
+    picture.src = images[active].img;
+}
+setInterval(changeImage, time)
